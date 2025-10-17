03a55adae1a717a7ff0cb2aea5be8d8bf644dea8
Alex <Alexvvv9511@yandex.ru>
2025-10-17 13:28:15 +0300

    Add searchBar, add FAB

--- app/src/main/java/com/example/rickandmortyapp/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortyapp/MainActivity.kt
@@ -3,54 +3,152 @@ package com.example.rickandmortyapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.rickandmortyapp.ui.theme.RickAndMortyAppTheme
+import com.example.rickandmortyapp.utils.search
 
 class MainActivity : ComponentActivity() {
-
-    val rick = Person(
-        id = 1,
-        name = "Rick Sanchez",
-        status = "Alive",
-        species = "Human",
-        gender = "Male",
-        image = R.drawable.rick
+    val usersList = listOf(
+        Person(0, "Mary"),
+        Person(1, "Patricia"),
+        Person(2, "Jennifer"),
+        Person(3, "Linda"),
+        Person(4, "Elizabeth"),
+        Person(5, "Barbara"),
+        Person(6, "Susan"),
+        Person(7, "Jessica"),
+        Person(8, "Sarah"),
+        Person(9, "Skyler"),
+        Person(10, "Nancy"),
+        Person(11, "Lisa"),
+        Person(12, "Betty"),
+        Person(13, "Sandra"),
+        Person(14, "Donna"),
+        Person(15, "Carol"),
+        Person(16, "Sharon"),
+        Person(17, "Michelle")
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ShowPersonList()
+            RickAndMortyAppTheme {
+                ShowSearch()
+            }
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun ShowSearch() {
+        val searchText = remember { mutableStateOf("") }
+        val mainList = remember { mutableStateOf(usersList) }
+        val isActive = remember { mutableStateOf(false) }
+
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            SearchBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                query = searchText.value,
+                onQueryChange = { text ->
+                    searchText.value = text
+                    mainList.value = search(text, usersList)
+                },
+                onSearch = { text ->
+                    isActive.value = false
+                },
+                placeholder = { Text(stringResource(R.string.search_characters)) },
+                active = isActive.value,
+                onActiveChange = {
+                    isActive.value = it
+                },
+                trailingIcon = {
+                    if (searchText.value.isNotEmpty()) {
+                        IconButton(
+                            onClick = {
+                                searchText.value = ""
+                                mainList.value = usersList
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.Close,
+                                contentDescription = "Clear search"
+                            )
+                        }
+                    } else {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "Search"
+                        )
+                    }
+                }
+            ) { PersonList(search(searchText.value, usersList)) }
+            PersonList(mainList.value)
         }
     }
 
     @Composable
-    fun ShowPersonList() {
-        RickAndMortyAppTheme {
+    fun PersonList(list: List<Person>) {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        )
+        {
             LazyVerticalGrid(
                 columns = GridCells.Fixed(2),
                 contentPadding = PaddingValues(8.dp),
                 verticalArrangement = Arrangement.spacedBy(8.dp),
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
             ) {
-                items(20) {
-                    PersonCard(person = rick)
+                items(items = list, key = { it.id }
+                ) { person ->
+                    PersonCard(person)
                 }
+            }
+            FloatingActionButton(
+                onClick = {},
+                shape = CircleShape,
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp)
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.filter_icon),
+                    contentDescription = null
+                )
             }
         }
     }
 }
+
+
 
--- app/src/main/java/com/example/rickandmortyapp/Person.kt
+++ app/src/main/java/com/example/rickandmortyapp/Person.kt
@@ -1,11 +1,10 @@
 package com.example.rickandmortyapp
 
 data class Person(
-    val id: Int,
-    val name: String,
-    val status: String,
-    val species: String,
-    val gender: String,
-    val image: Int
+    val id: Int = 1,
+    val name: String = "Rick Sanchez",
+    val status: String = "Alive",
+    val species: String = "Human",
+    val gender: String = "Male",
+    val image: Int = R.drawable.rick,
 )
-
